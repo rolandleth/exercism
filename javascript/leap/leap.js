aaf1c8fd1a11ce63bbe98f2021b4882089a9de2c
Roland Leth <roland@leth.ro>
2019-03-16 10:41:31 +0200

    Improved leap

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,7 +1,3 @@
 export const isLeap = (year) => {
-	if (year % 400 === 0) {
-		return true
-	}
-
-	return year % 4 === 0 && year % 100 > 0
+	return year % 400 === 0 || (year % 4 === 0 && year % 100 > 0)
 }
