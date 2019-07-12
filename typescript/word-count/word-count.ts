fa7d9e29aefd845d11a761567da6a8800a0f71c0
Roland Leth <roland@leth.ro>
2019-07-12 11:56:41 +0300

    Improved word count

--- typescript/word-count/word-count.ts
+++ typescript/word-count/word-count.ts
@@ -1,15 +1,11 @@
 export default class Words {
-  count = (phrase: string) => {
-    const result: Map<string, number> = new Map()
-
-    phrase
+  count = (phrase: string): Map<string, number> => {
+    return phrase
       .trim()
-      .replace(/[\s\t\n]+/, " ")
-      .split(" ")
-      .forEach((e) =>
-        result.set(e.toLowerCase(), (result.get(e.toLowerCase()) || 0) + 1)
-      )
-
-    return result
+      .toLowerCase()
+      .split(/\s+/)
+      .reduce((accumulator, word) => {
+        return accumulator.set(word, (accumulator.get(word) || 0) + 1)
+      }, new Map())
   }
 }
